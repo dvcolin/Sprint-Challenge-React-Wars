fa53b626073f5dcd2d0bbaff0a7376525c0ca869
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-07-19 10:51:57 -0700

    Added colors and fonts in their own components. Added colors and fonts to styled components

---
+++ starwars/src/components/Fonts.js
@@ -0,0 +1,4 @@
+export const fonts = {
+    mainFont: `'Play', sans-serif;`,
+    secondaryFont: `'Roboto Condensed', sans-serif;`,
+}
--- starwars/src/components/PersonContent.js
+++ starwars/src/components/PersonContent.js
@@ -1,36 +1,32 @@
 import styled from 'styled-components';
+import { colors } from './Colors';
+import {fonts } from './Fonts';
 
 export const PersonName = styled.h2`
     font-size: 2.5rem;
     color: white;
     margin-bottom: 0;
+    font-family: ${fonts.mainFont};
 `;
 
 export const PersonHeight = styled.p`
-    font-size: 1.6rem;
-    margin-right: auto;
-    margin-left: 1.6rem;
-    margin-bottom: 0;
-    margin-top: 0.5rem;
+    font-size: 1.4rem;
+    color: ${colors.neon};
+    font-family: ${fonts.secondaryFont};
 `;
 export const PersonMass = styled.p`
-    font-size: 1.6rem;
-    margin-right: auto;
-    margin-left: 1.6rem;
-    margin-bottom: 0;
-    margin-top: 0.5rem;
+    font-size: 1.4rem;
+    color: ${colors.lightBlue};
+    font-family: ${fonts.secondaryFont};
 `;
 export const PersonGender = styled.p`
-    font-size: 1.6rem;
-    margin-right: auto;
-    margin-left: 1.6rem;
-    margin-bottom: 0;
-    margin-top: 0.5rem;
+    font-size: 1.4rem;
+    color: ${colors.lightYellow};
+    font-family: ${fonts.secondaryFont};
+    
 `;
 export const PersonBirthYear = styled.p`
-    font-size: 1.6rem;
-    margin-right: auto;
-    margin-left: 1.6rem;
-    margin-bottom: 0;
-    margin-top: 0.5rem;
+    font-size: 1.4rem;
+    color: ${colors.lightPurple};
+    font-family: ${fonts.secondaryFont};
 `;
